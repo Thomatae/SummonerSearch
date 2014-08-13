5e891b42125e83071be3e6264365f445d9a9a884
Trever Thomas <trever.thomas@banno.com>
2014-08-13 08:58:00 -0500

    gradle changes

---
+++ summoners/src/main/java/com/league2/app/Module/ActivityModule.java
@@ -0,0 +1,8 @@
+package com.league2.app.Module;
+
+import dagger.Module;
+import com.league2.app.Fragments.Homepage;
+
+@Module(injects = Homepage.class, library = true, complete = false)
+public class ActivityModule {
+}
---
+++ summoners/src/main/java/com/league2/app/Module/TaskModule.java
@@ -0,0 +1,8 @@
+package com.league2.app.Module;
+
+import dagger.Module;
+import com.league2.app.Service.HomePageRetrievalTask;
+
+@Module(injects = HomePageRetrievalTask.class, library = true, complete = false)
+public class TaskModule {
+}
